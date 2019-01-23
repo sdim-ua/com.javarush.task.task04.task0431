ba12e919908f4fb5b92eb4d85389678139a7e4c5
sdim-ua <ingersol@yandex.ua>
2019-01-24 00:15:19 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,18 @@
+/* 
+От 10 до 1
+*/
+
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        int count = 10;
+        while (count > 0 ){
+            System.out.println(count);
+            count --;
+        }
+        
+        
+    }
+}
